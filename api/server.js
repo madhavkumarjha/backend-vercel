7f1c42d576cc357d70762bd5edfa97de45acd454
madhav kumar <madhavxman@gmail.com>
2026-06-28 11:52:59 +0530

    some

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // Aapke routes
 app.get('/api/server', (req, res) => {
   res.send('Hello from Express on Vercel!');
+});
+app.get('/', (req, res) => {
+  res.send(process.env.LINK);
 });
 
 // ZAROORI: Express app ko export karein
